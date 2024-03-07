55958fa42081f53a80e04debbc4991b123e1a7c7
Gairolarishav <101463798+Gairolarishav@users.noreply.github.com>
2024-03-07 17:16:30 +0530

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,28 +1,30 @@
 /// Ajax Form Submission
 $(document).ready(function() {
     // Handle form submission
+    $('#submitButton').show()
+    $('.spin-loader').hide()
+
     $('#submitButton').click(function() {
         var textInput = $('input[name="fname"]').val(); // Get text input value
-        var imageCount = $('#count').val(); // Get dropdown input value
-        var imagesize = $('#size').val(); // Get dropdown input value
 
+        
+        $('.spin-loader').show()
+        $('#submitButton').hide()
         // Prepare data object
         var formData = {
             text: textInput,
-            count: imageCount,
-            size : imagesize
         };
-
-        // Clear previous image cards
-        $('#imageCardsContainer').empty();
 
         // Show loading message
         $('#loadingMessage').show();
 
+        // Clear previous image cards
+        $('#imageCardsContainer').empty();
+
         // Send AJAX request
         $.ajax({
             type: 'POST',
-            url: 'https://image-generator-api-3jrs.onrender.com/image_generator',
+            url: 'http://localhost:8000/image_generator',
             contentType: 'application/json',
             data: JSON.stringify(formData),
             success: function(response) {
@@ -30,23 +32,27 @@ $(document).ready(function() {
                 console.log('Data submitted successfully:', response);
 
                 $('#loadingMessage').hide();
+                $('.spin-loader').hide()
+                $('#submitButton').show()
 
-                // check if user enter input or not
-                if (response=='Please Enter something') {
-                       var cardHtml = '<div class="col-sm-12">' +
-                    '<div class="card px-3 text-dark font-weight-normal">' +
-                    '<h5 style="font-weight:400 !important;font-size:1rem !important">' + response + '</h5>' +
-                    '</div>' +
-                    '</div>';
-                     $('#imageCardsContainer').append(cardHtml);
+                if (typeof response == 'string' ) {
+                    console.log('not a list :' ,response)
+                    var cardHtml = '<div class="col-sm-12">' +
+                 '<div class="card px-3 text-dark font-weight-normal">' +
+                 '<h5 style="font-weight:400 !important;font-size:1rem !important">' + response + '</h5>' +
+                 '</div>' +
+                 '</div>';
+                  $('#imageCardsContainer').append(cardHtml);
+                  
                 }
                 else{// Create image cards for each URL in the response
-                    response.forEach(function(url, index) {
+                    response.forEach(function(image, index) {
+                        console.log('list:' ,image)
                         var cardHtml = '<div class="col-sm-3">' +
                             '<div class="card">' +
-                            '<img src="data:image/png;base64,' + url + '" class="card-img-top" alt="...">' +
+                            '<img src="data:image/png;base64,' + image + '" class="card-img-top" alt="...">' +
                             '<div class="card-body">' +
-                            '<a href="data:image/png;base64,' + url + '" download="' + index + '.png"><button class="btn btn-primary download-btn">Download</button></a>' +
+                            '<a href="data:image/png;base64,' + image + '" download="' + index + '.png"><button class="btn btn-primary download-btn">Download</button></a>' +
                             '</div>' +
                             '</div>' +
                             '</div>';
@@ -59,15 +65,20 @@ $(document).ready(function() {
             },
             error: function(xhr, status, error) {
                 // Handle error response
-                console.log('Error:', error);
-
+                var errorMessage
+                if (xhr.status === 403) {
+                    // Handle Bing blocking the prompt
+                    errorMessage = "blocked the prompt. Please revise it and try again.";
+                } else {
+                    // Handle other errors (e.g., internal server error)
+                    errorMessage= "Internal Server Error";
+                }
                 // Hide loading message
                 $('#loadingMessage').hide();
-                // Clear previous image cards
-                $('#imageCardsContainer').empty();
 
-                // Display error message
-                var errorMessage = xhr.responseJSON && xhr.responseJSON.detail ? xhr.responseJSON.detail : 'Something went wrong try after few minutes';
+                $('.spin-loader').hide()
+                $('#submitButton').show()
+
                 var cardHtml = '<div class="col-sm-12">' +
                     '<div class="card px-3 text-dark font-weight-normal">' +
                     '<h5 style="font-weight:400 !important;font-size:1rem !important">' + errorMessage + '</h5>' +
