06258dff232a3cd0689dcf8e4e8ca98a1a472fd5
Gairolarishav <101463798+Gairolarishav@users.noreply.github.com>
2024-03-07 12:38:29 +0530

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,24 +1,33 @@
 /// Ajax Form Submission
 $(document).ready(function() {
     // Handle form submission
+    $('#submitButton').show()
+    $('.spin-loader').hide()
+
     $('#submitButton').click(function() {
         var textInput = $('input[name="fname"]').val(); // Get text input value
-
+        var imageCount = $('#count').val(); // Get dropdown input value
+        var imagesize = $('#size').val(); // Get dropdown input value
+        
+        $('.spin-loader').show()
+        $('#submitButton').hide()
         // Prepare data object
         var formData = {
             text: textInput,
+            count: imageCount,
+            size : imagesize
         };
-
-        // Clear previous image cards
-        $('#imageCardsContainer').empty();
 
         // Show loading message
         $('#loadingMessage').show();
 
+        // Clear previous image cards
+        $('#imageCardsContainer').empty();
+
         // Send AJAX request
         $.ajax({
             type: 'POST',
-            url: 'https://image-generator-api-3jrs.onrender.com/image_generator',
+            url: 'https://image-and-content-generator-website.onrender.com//image_generator',
             contentType: 'application/json',
             data: JSON.stringify(formData),
             success: function(response) {
@@ -26,23 +35,27 @@ $(document).ready(function() {
                 console.log('Data submitted successfully:', response);
 
                 $('#loadingMessage').hide();
+                $('.spin-loader').hide()
+                $('#submitButton').show()
 
-                // check if user enter input or not
-                if (response=='Please Enter something') {
-                       var cardHtml = '<div class="col-sm-12">' +
-                    '<div class="card px-3 text-dark font-weight-normal">' +
-                    '<h5 style="font-weight:400 !important;font-size:1rem !important">' + response + '</h5>' +
-                    '</div>' +
-                    '</div>';
-                     $('#imageCardsContainer').append(cardHtml);
+                if (typeof response == 'string' ) {
+                    console.log('not a list :' ,response)
+                    var cardHtml = '<div class="col-sm-12">' +
+                 '<div class="card px-3 text-dark font-weight-normal">' +
+                 '<h5 style="font-weight:400 !important;font-size:1rem !important">' + response + '</h5>' +
+                 '</div>' +
+                 '</div>';
+                  $('#imageCardsContainer').append(cardHtml);
+                  
                 }
                 else{// Create image cards for each URL in the response
-                    response.forEach(function(url, index) {
+                    response.forEach(function(image, index) {
+                        console.log('list:' ,image)
                         var cardHtml = '<div class="col-sm-3">' +
                             '<div class="card">' +
-                            '<img src="data:image/png;base64,' + url + '" class="card-img-top" alt="...">' +
+                            '<img src="data:image/png;base64,' + image + '" class="card-img-top" alt="...">' +
                             '<div class="card-body">' +
-                            '<a href="data:image/png;base64,' + url + '" download="' + index + '.png"><button class="btn btn-primary download-btn">Download</button></a>' +
+                            '<a href="data:image/png;base64,' + image + '" download="' + index + '.png"><button class="btn btn-primary download-btn">Download</button></a>' +
                             '</div>' +
                             '</div>' +
                             '</div>';
@@ -59,8 +72,9 @@ $(document).ready(function() {
 
                 // Hide loading message
                 $('#loadingMessage').hide();
-                // Clear previous image cards
-                $('#imageCardsContainer').empty();
+
+                $('.spin-loader').hide()
+                $('#submitButton').show()
 
                 // Display error message
                 var errorMessage = xhr.responseJSON && xhr.responseJSON.detail ? xhr.responseJSON.detail : 'Something went wrong try after few minutes';
