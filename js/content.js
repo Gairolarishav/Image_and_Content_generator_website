9c2ba73817eb77652cbe44eff09e8ec89123d806
Gairolarishav <101463798+Gairolarishav@users.noreply.github.com>
2024-03-19 11:54:31 +0530

    Update content.js

--- js/content.js
+++ js/content.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
         // Send AJAX request
         $.ajax({
             type: 'POST',
-            url: 'https://image-generator-api-3jrs.onrender.com/content_generator',
+            url: 'https://image-generator-api-myfirstimagepush-1.onrender.com/content_generator',
             contentType: 'application/json',
             data: JSON.stringify(formData),
             success: function(response) {
